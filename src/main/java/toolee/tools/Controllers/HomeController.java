b6c574a72b9e6f2eed3bd8f9741e9354871a99d7
williams osunkwo <wosunkwo@gmail.com>
2019-07-29 12:34:00 -0700

    finished discover route and register route

--- src/main/java/toolee/tools/Controllers/HomeController.java
+++ src/main/java/toolee/tools/Controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+@Controller
 public class HomeController {
     @Autowired
     UserRepository userRepository;
